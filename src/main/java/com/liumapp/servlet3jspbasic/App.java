4bf7e5d96ecd6e9d6da3014e29323bbb23121a5a
liumapp <liumapp.com@gmail.com>
2017-06-21 21:38:37 +0800

    init project

---
+++ src/main/java/com/liumapp/servlet3jspbasic/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.servlet3jspbasic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
