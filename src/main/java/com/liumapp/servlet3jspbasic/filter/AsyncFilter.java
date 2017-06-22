7a248ecbd02e42b068c2e087ea4a224467c6c4e3
liumapp <liumapp.com@gmail.com>
2017-06-22 09:54:33 +0800

    添加async的filter

---
+++ src/main/java/com/liumapp/servlet3jspbasic/filter/AsyncFilter.java
@@ -0,0 +1,27 @@
+package com.liumapp.servlet3jspbasic.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by liumapp on 6/22/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@WebFilter(filterName = "AsyncFilter" , value = {"/index.jsp"} , dispatcherTypes = {DispatcherType.ASYNC , DispatcherType.REQUEST})
+public class AsyncFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("AsyncFilter started");
+        filterChain.doFilter(servletRequest , servletResponse);
+        System.out.println("AsyncFilter ended");
+    }
+
+    public void destroy() {
+
+    }
+}
